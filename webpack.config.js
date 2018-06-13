250d572d82f8478505a7260d5e1777c9f88e0160
awhy <17600405632@163.com>
2018-06-13 20:19:37 +0800

    save

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,54 @@
-const webpack  = require('webpack')
+const webpack = require('webpack')
 const htmlWebpackPlugin = require('html-webpack-plugin')
 const cleanWebpackPlugin = require('clean-webpack-plugin')
 module.exports = {
-    entry: __dirname +"/app/main.js",
-    output: {
-        path: __dirname + "/build",
-        filename: "bundle.js"
-    },
-    devtool: "eval-source-map",
-    devServer: {
-        contentBase: "./public",
-        historyApiFallback: true,
-        inline: true
-    },
-    module: {
-        rules: [
-            {
-                test: /(\.jsx|\.js)$/,
-                use: {
-                    loader: "babel-loader",
-                },
-                exclude: /node_modu.les/
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    },{
-                        loader: "css-loader",
-                        options: {
-                            module: true,
-                            localIdentName: "[name]__[local]--[hash:base64:5]"
-                        }
-
-                    }
-                ]
+  entry: __dirname + "/app/main.js",
+  output: {
+    path: __dirname + "/build",
+    filename: "bundle.js"
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    contentBase: "./public",
+    historyApiFallback: true,
+    inline: true
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        use: {
+          loader: "babel-loader",
+        },
+        exclude: /node_modu.les/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          }, {
+            loader: "css-loader",
+            options: {
+              module: true,
+              localIdentName: "[name]__[local]--[hash:base64:5]"
             }
+
+          }
         ]
-    },
-    plugins: [
-        new webpack.BannerPlugin('awhy的webpack study'),
-        new webpack.HotModuleReplacementPlugin(),
-        new htmlWebpackPlugin({
-            template: __dirname+'/app/index.tmpl.html'
-        }),
-        new cleanWebpackPlugin('build/*.*',{
-            root: __dirname,
-            verbose: true,
-            dry: false
-        })
+      }
     ]
+  },
+  plugins: [
+    new webpack.BannerPlugin('awhy的webpack study'),
+    new webpack.HotModuleReplacementPlugin(),
+    new htmlWebpackPlugin({
+      template: __dirname + '/app/index.tmpl.html'
+    }),
+    new cleanWebpackPlugin('build/*.*', {
+      root: __dirname,
+      verbose: true,
+      dry: false
+    })
+  ]
 }
